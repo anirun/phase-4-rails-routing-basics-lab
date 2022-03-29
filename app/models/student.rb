7ce9ba6b178a47d0d1d622a8b7b752fe165a3b48
Nurina Junus <nurina.junus@gmail.com>
2022-03-28 19:28:41 -0600

    commit tree 7a23675781387ff49aceea70ca316c43524262fc

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
 
+  def index
+    students = Student.all
+    render json: students
+  end
+
+  def grades
+    grades = Student.all.map{ |student| student.grade}
+    render json: grades
+  end
+  
   def to_s
     "#{first_name} #{last_name}"
   end
